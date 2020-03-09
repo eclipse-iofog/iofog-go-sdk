7130b955c1b525ef6f109418396d6dc9d080b8bf
SergeRadinovich <serge@edgeworx.io>
2020-03-10 12:06:02 +1300

    Add k8s package

--- go.mod
+++ go.mod
@@ -5,8 +5,39 @@ go 1.12
 require (
 	github.com/eapache/channels v1.1.0
 	github.com/eapache/queue v1.1.0 // indirect
+	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/golang/protobuf v1.3.4 // indirect
+	github.com/google/btree v1.0.0 // indirect
+	github.com/googleapis/gnostic v0.4.1 // indirect
 	github.com/gorilla/websocket v1.4.1
+	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79 // indirect
+	github.com/imdario/mergo v0.3.8 // indirect
 	github.com/json-iterator/go v1.1.8
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/crypto v0.0.0-20200302210943-78000ba7a073 // indirect
+	golang.org/x/net v0.0.0-20200301022130-244492dfa37a // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
+	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	k8s.io/api v0.17.3
+	k8s.io/apimachinery v0.17.3
+	k8s.io/client-go v11.0.0+incompatible
 	k8s.io/gengo v0.0.0-20191108084044-e500ee069b5c // indirect
+	k8s.io/utils v0.0.0-20200229041039-0a110f9eb7ab // indirect
+	sigs.k8s.io/yaml v1.2.0 // indirect
+)
+
+// Pinned to kubernetes-1.13.4
+replace (
+	k8s.io/api => k8s.io/api v0.0.0-20190222213804-5cb15d344471
+	k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver v0.0.0-20190228180357-d002e88f6236
+	k8s.io/apimachinery => k8s.io/apimachinery v0.0.0-20190221213512-86fb29eff628
+	k8s.io/apiserver => k8s.io/apiserver v0.0.0-20190228174905-79427f02047f
+	k8s.io/cli-runtime => k8s.io/cli-runtime v0.0.0-20190228180923-a9e421a79326
+	k8s.io/client-go => k8s.io/client-go v0.0.0-20190228174230-b40b2a5939e4
+	k8s.io/code-generator => k8s.io/code-generator v0.0.0-20181117043124-c2090bec4d9b
+	k8s.io/kube-aggregator => k8s.io/kube-aggregator v0.0.0-20190228175259-3e0149950b0e
+	k8s.io/kube-openapi => k8s.io/kube-openapi v0.0.0-20180711000925-0cf8f7e6ed1d
+	k8s.io/kubernetes => k8s.io/kubernetes v1.13.4
 )
---
+++ pkg/k8s/wait.go
@@ -0,0 +1,102 @@
+/*
+ *  *******************************************************************************
+ *  * Copyright (c) 2019 Edgeworx, Inc.
+ *  *
+ *  * This program and the accompanying materials are made available under the
+ *  * terms of the Eclipse Public License v. 2.0 which is available at
+ *  * http://www.eclipse.org/legal/epl-2.0
+ *  *
+ *  * SPDX-License-Identifier: EPL-2.0
+ *  *******************************************************************************
+ *
+ */
+
+ package k8s
+
+ import (
+	 "errors"
+	 "strings"
+ 
+	 "k8s.io/api/core/v1"
+	 metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+ )
+ 
+ func (cl *Client) WaitForLoadBalancer(namespace, name string, timeoutSeconds int64) (ip string, err error) {
+	 // Get watch handler to observe changes to services
+	 watch, err := cl.CoreV1().Services(namespace).Watch(metav1.ListOptions{TimeoutSeconds: &timeoutSeconds})
+	 if err != nil {
+		 return
+	 }
+ 
+	 // Wait for Services to have IPs allocated
+	 for event := range watch.ResultChan() {
+		 if event.Type == "Error" || event.Type == "Deleted" {
+			 err = errors.New("Could not wait for service " + namespace + "/" + name)
+			 return
+		 }
+		 svc, ok := event.Object.(*v1.Service)
+		 if !ok {
+			 err = errors.New("Could not wait for service " + namespace + "/" + name)
+			 return
+		 }
+ 
+		 // Ignore irrelevant service events
+		 if svc.Name != name {
+			 continue
+		 }
+		 // Loadbalancer must be ready
+		 if len(svc.Status.LoadBalancer.Ingress) == 0 {
+			 continue
+		 }
+ 
+		 ip = svc.Status.LoadBalancer.Ingress[0].IP
+		 watch.Stop()
+	 }
+ 
+	 return
+ }
+ 
+ func (cl *Client) WaitForPod(namespace, name string, timeoutSeconds int64) error {
+	 // Get watch handler to observe changes to pods
+	 watch, err := cl.CoreV1().Pods(namespace).Watch(metav1.ListOptions{TimeoutSeconds: &timeoutSeconds})
+	 if err != nil {
+		 return err
+	 }
+ 
+	 // Wait for pod events
+	 for event := range watch.ResultChan() {
+		 if event.Type == "Error" || event.Type == "Deleted" {
+			 return errors.New("Failed to wait for pod " + namespace + "/" + name)
+		 }
+		 // Get the pod
+		 pod, ok := event.Object.(*v1.Pod)
+		 if !ok {
+			 return errors.New("Failed to wait for pod " + namespace + "/" + name)
+		 }
+		 // Check pod is in running state
+		 splitName := strings.Split(pod.Name, "-")
+		 if len(splitName) <= 2 {
+			 continue
+		 }
+		 splitName = splitName[0 : len(splitName)-2]
+		 joinName := strings.Join(splitName, "-")
+		 if joinName != name {
+			 continue
+		 }
+ 
+		 if pod.Status.Phase == "Running" {
+			 ready := true
+			 for _, cond := range pod.Status.Conditions {
+				 if cond.Status != "True" {
+					 ready = false
+					 break
+				 }
+			 }
+			 if ready {
+				 watch.Stop()
+			 }
+		 }
+	 }
+	 return nil
+ }
+ 
